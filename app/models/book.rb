e3e3dd68b93e03a9905fc2f88bb562f70046ac72
anhvan94 <nguyenanhvan1511@gmail.com>
2019-09-10 18:25:27 +0700

    feat(webapp):show_books

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,9 @@
 class Book < ApplicationRecord
-    belongs_to :category
+  belongs_to :category
+  validates :category_id, presence: true
+  validates :title, presence: true
+  validates :author, presence: true, length: {maximum: 100}
+  validates :conten, presence:true
+
+
 end
